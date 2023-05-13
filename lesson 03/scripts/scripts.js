efef1cddb991b24fc7155d45ebd75cb8dff3f633
jessochs <fac17002@byui.edu>
2023-05-13 13:34:10 -0700

    menu

--- lesson 03/scripts/scripts.js
+++ lesson 03/scripts/scripts.js
@@ -1,3 +1,7 @@
 let date = new Date();
 document.querySelector("#year").textContent = date.getFullYear();
 document.querySelector("#last-updated").textContent = document.lastModified;
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
