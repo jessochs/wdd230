ead723ab54b170e4e43fc3b6b5c3145de69761d1
jessochs <fac17002@byui.edu>
2023-05-15 22:16:03 -0700

    ham button

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,12 @@
 let date = new Date();
 document.querySelector("#year").textContent = date.getFullYear();
 document.querySelector("#last-updated").textContent = document.lastModified;
+document.querySelector("time").textContent= new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(date);
+const hamButton = document.querySelector('#hamButton');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('responsive');
+});
