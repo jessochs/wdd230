a9a75bef6c820275b04af0d679da404380422a46
jessochs <fac17002@byui.edu>
2023-06-02 08:28:10 -0700

    discover page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -17,3 +17,43 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('responsive');
 });
+let visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = "This is your first visit!";
+}
+
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
+todayDisplay.textContent = Date.now();
+
+let imagesThatLoad = document.querySelectorAll("[data-src]");
+let loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+
+if ("IntersectionObserver" in window) {
+    let observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesThatLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesThatLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
