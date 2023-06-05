3e993fd28b00dafae426b7f560ecc855092f2e0b
jessochs <fac17002@byui.edu>
2023-06-05 16:58:16 -0700

    made changes

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -3,9 +3,11 @@ let weekday = date.getDay();
 let message;
 
 if (weekday >= 2 && weekday <= 3) {
-	message = "Please join us for the Chamber Meet and Greet on Wednesday at 7:00 pm."
-}
+	
+message = "Please join us for the Chamber Meet and Greet on Wednesday at 7:00 pm."
 document.querySelector("#message").textContent = message;
+} 
+
 document.querySelector("#year").textContent = date.getFullYear();
 document.querySelector("#last-updated").textContent = document.lastModified;
 document.querySelector("#time").textContent= new Intl.DateTimeFormat("en-UK", {
@@ -17,19 +19,19 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('responsive');
 });
-let visitsDisplay = document.querySelector(".visits");
+// let visitsDisplay = document.querySelector(".visits");
 
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
+// let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = "This is your first visit!";
-}
+// if (numVisits !== 0) {
+// 	visitsDisplay.textContent = numVisits;
+// } else {
+// 	visitsDisplay.textContent = "This is your first visit!";
+// }
 
-numVisits++;
-localStorage.setItem("visits-ls", numVisits);
-todayDisplay.textContent = Date.now();
+// numVisits++;
+// localStorage.setItem("visits-ls", numVisits);
+// todayDisplay.textContent = Date.now();
 
 let imagesThatLoad = document.querySelectorAll("[data-src]");
 let loadImages = (image) => {
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,10 +1,10 @@
-let temperature = 50;
-let windSpeed = 2;
+let temperature = document.querySelector("#temp").textContent;
+let windSpeed = document.querySelector("#speed").textContent;
 
 let windChill;
 
-if(temperature <= 50 && windSpeed < 3) {
-    windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+if(temperature <= 50 && windSpeed > 3) {
+    windChill = Math.floor(35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16));
 
 }
 else {
@@ -12,7 +12,7 @@ else {
 }
 
 document.querySelector("#windChill").textContent = windChill;
-document.querySelector("#temp").textContent = temperature;
+
 
 
  
