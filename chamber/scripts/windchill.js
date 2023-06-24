bc17c1f5c0a2477ed7d2dedb0ce4eac2ae412feb
jessochs <fac17002@byui.edu>
2023-06-24 12:22:48 -0700

    weather added

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,5 +1,5 @@
-let temperature = document.querySelector("#temp").textContent;
-let windSpeed = document.querySelector("#speed").textContent;
+let temperature = document.querySelector("#temp");
+let windSpeed = document.querySelector("#speed");
 
 let windChill;
 
@@ -13,6 +13,42 @@ else {
 
 document.querySelector("#windChill").textContent = windChill;
 
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=33.5387&lon=-112.1860&units=imperial&appid=d7bb0dd92a04238853fc9784824a1e33';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  }
+
+  function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
 
  
